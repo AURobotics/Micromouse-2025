3ba245758a1fc1c1c858504299818c7e5f484813
Mamdouh_Nafee <mamdouhdido5@gmail.com>
2025-02-26 11:40:41 +0200

    Update Algorithm.cpp

--- Software/Algorithm.cpp
+++ Software/Algorithm.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 #include <cstdlib>
-// #include "API.h" // micromouse simulator api
+#include "API.h" // micromouse simulator api
 
 #define MAX_H 18
 #define MAX_W 18
+
+
+void log(const std::string& text) {     /// copied that from the mms example 
+    std::cerr << text << std::endl;
+}
+
+std::string tostr(short x)
+{
+    //log("tostr");
+    if(x == 0)return "0";
+    std::string ret = "";
+    while(x)
+    {
+        ret = char((x%10)+'0') + ret;
+        x/=10;
+    }
+    return ret;
+}
+
+
+
 // queue implementation
 typedef struct
 {
@@ -20,6 +41,21 @@ queue *initialise(short size)
    q->items=(char*)malloc(size*sizeof(char));
    return(q);
 }
+bool isfull(queue*q)
+{
+    if(q->counter==q->size)
+        return(1);
+    else
+        return(0);
+}
+bool isempty(queue*q)
+{
+    if(q->counter==0)
+        return(1);
+    else
+        return(0);
+}
+
 void enqueue(queue*q, char value)
 {
     if(!isfull(q))
@@ -40,24 +76,11 @@ char dequeue(queue*q)
         return result;
     }
 }
-bool isfull(queue*q)
-{
-    if(q->counter==q->size)
-        return(1);
-    else
-        return(0);
-}
-bool isempty(queue*q)
-{
-    if(q->counter==0)
-        return(1);
-    else
-        return(0);
-}
+
 //end of queue implementation
 
 char curr_dir = 0; // 0--> North, 1 --> East, 2 --> South, 3 --> West
-char curr_r = 0, curr_c = 0;
+char curr_r = 16, curr_c = 1;
 
 
 bool maze [MAX_H][MAX_W][5] = {0}; // represents the maze, first 4 bits represent the walls N E S W, the last bit represents the visiting status
@@ -84,16 +107,41 @@ short dis [MAX_H][MAX_W] = {
     {16, 15, 14, 13, 12, 11, 10, 9, 8, 8, 9, 10, 11, 12, 13, 14, 15, 16}
 };
 
+void update_mms_maze()
+{
+    for(int i=1;i<=16;i++)
+    {
+        for(int j=1;j<=16;j++)
+        {
+            API::setText(j-1,16-i,tostr(dis[i][j]));
+        }
+    }
+}
+
+
+void set_wall()
+{
+    std::cerr<<"\ncoloring walls"<<curr_c-1<<" "<<16-curr_r<<std::endl;
+    for(int i=0;i<4;i++) std::cerr<<maze[curr_r][curr_c][i]<<" ";
+    std::cerr<<std::endl;
+    if(maze[curr_r][curr_c][0])API::setWall(curr_c-1,16-curr_r,'n');
+    if(maze[curr_r][curr_c][1])API::setWall(curr_c-1,16-curr_r,'e');
+    if(maze[curr_r][curr_c][2])API::setWall(curr_c-1,16-curr_r,'s');
+    if(maze[curr_r][curr_c][3])API::setWall(curr_c-1,16-curr_r,'w');
+}
+
+
+
 // change r, c to move to: N, E, S, W
-signed char r_mov [4] =   {1, 0,-1, 0};
+signed char r_mov [4] =   {-1, 0,1, 0};
 signed char c_mov [4] =   {0, 1, 0,-1};
 
 bool isValid (char r, char c) {
-    return ((r >= 0) && (r <= MAX_H)) && ((c >= 0) && (c <= MAX_W));
+    return ((r >= 0) && (r < MAX_H)) && ((c >= 0) && (c < MAX_W));
 }
 
-bool isAccessible (char dir) {
-    return ! maze [curr_r][curr_c];
+bool isAccessible (char r, char c,char dir) {
+    return ! (maze [r][c][dir] || maze[r+r_mov[dir]][c + c_mov[dir]][(dir+2)%4]);
 }
 queue*c_q=initialise(MAX_H); //queue initialisation for storing row and coloumn
 queue*r_q=initialise(MAX_W);
@@ -114,34 +162,41 @@ void flood () {
         char r=dequeue (r_q);
         char c=dequeue (c_q);
         for (int i = 0; i < 4; i ++) {
-            if (isAccessible(i)&&isValid(r,c)&&dis [r + r_mov [i]][c + c_mov [i]]==-1) {
+            if (isValid(r + r_mov[i],c + c_mov[i])&&isAccessible(r,c,i)&&dis [r + r_mov [i]][c + c_mov [i]]==-1) {
                 dis [r + r_mov [i]][c + c_mov [i]] = dis [r][c] + 1;
                 enqueue (r_q,r + r_mov [i]);
                 enqueue (c_q,c + c_mov [i]);
             }
         }
     }
+
+
+    update_mms_maze();
+
 }
 
 void moveTo (char r, char c) {
     // get where I want to move relative to abolute direction (y3ny lw el robot bases north) ana lesa m2alef el term dah
-    int dir;
-    if(c < curr_c) dir = 0;
-    if(c > curr_c) dir = 2;
-    if(r < curr_r) dir = 3;
-    if(r > curr_r) dir = 1;
-
+    short dir;
+    if(r < curr_r) dir = 0;
+    if(r > curr_r) dir = 2;
+    if(c < curr_c) dir = 3;
+    if(c > curr_c) dir = 1;    
+    std::cerr << (int)r<<" "<<(int)c << std::endl;
+    log("direction: " + tostr(dir));
     // compare the movement direction to the current directoin to know how should I turn 
 
-    if(dir - curr_dir == -1)// turn left
+    if(dir - curr_dir == -1 || dir-curr_dir == 3)// turn left
     {
         API::turnLeft();
         API::moveForward();
+        curr_dir += 3; // b3mel +3 msh -1 3shan el negative numbers don't work/work differently fel mod 
     } 
-    else if(dir - curr_dir == 1 ) // turn right 
-    {
+    else if(dir - curr_dir == 1 ||dir - curr_dir == -3) // turn right 
+    {   
         API::turnRight();
         API::moveForward();
+        curr_dir++;
     }
     else if(dir == curr_dir) // move forward 
     {
@@ -152,19 +207,31 @@ void moveTo (char r, char c) {
         API::turnRight();
         API::turnRight();
         API::moveForward();
+        curr_dir += 2;
     }
+
+    curr_dir %= 4;
+    curr_c = c;
+    curr_r = r;
 
 }
 
 void exploreToCenter () {
     while (! (((curr_r == MAX_H / 2 - 1) || (curr_r == MAX_H / 2)) && ((curr_c == MAX_W / 2 - 1) || (curr_c == MAX_W / 2)))) {
         maze [curr_r][curr_c][4] = 1;
-
+        log("start: " + tostr(dis[curr_r][curr_c]));
+        std::cerr << (int)curr_r<<" "<<(int)curr_c <<" "<<(int)curr_dir<< std::endl;
         bool walls [4];
         walls [0] = API::wallFront();
         walls [1] = API::wallRight();
-        walls [2] = API::wallBack();
+        //walls [2] = API::wallBack(); wall back isn't working
         walls [3] = API::wallLeft();
+        if(curr_r == 16 && curr_c == 1 && curr_dir == 0) walls[2] = 1;
+        else walls[2] = 0;
+        //for(int i=0;i<4;i++) std::cerr<<walls[i]<<" ";
+        //std::cerr<<std::endl;
+
+        
 
         char d = curr_dir, w = 0;
         do {
@@ -172,15 +239,17 @@ void exploreToCenter () {
             d = (d + 1) % 4;
             w ++;
         } while (d != curr_dir);
-
+        
+        set_wall();
         char next_r = curr_r, next_c = curr_c;
 
         for (char i = 0; i < 4; i ++) {
-            if (isValid (curr_r + r_mov [i], curr_c + c_mov [i]) && isAccessible (i) && dis [curr_r + r_mov [i]][curr_c + c_mov [i]] < dis [next_r][next_c]) {
+            if (isValid (curr_r + r_mov [i], curr_c + c_mov [i]) && isAccessible(curr_r,curr_c,i) && dis [curr_r + r_mov [i]][curr_c + c_mov [i]] < dis [next_r][next_c]) {
                 next_r = curr_r + r_mov [i];
                 next_c = curr_c + c_mov [i];
             }
         }
+        log("to: " + tostr(dis[next_r][next_c]));
 
         if ((next_r == curr_r) && (next_c == curr_c))
             flood ();
@@ -190,5 +259,6 @@ void exploreToCenter () {
 }
 
 main () {
+    update_mms_maze();
     exploreToCenter ();
 }
