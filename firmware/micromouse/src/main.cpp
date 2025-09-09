2cf88f83d93fed31d1be7bca5d43b518f1f864b0
minawashere <minasalama2580@gmail.com>
2025-09-10 00:54:56 +0300

    mehmeh^4

--- firmware/micromouse/src/main.cpp
+++ firmware/micromouse/src/main.cpp
@@ -6,6 +6,14 @@ enum class ADC_PINS : uint8_t {
     ADC1_0 = 1, ADC1_1, ADC1_2, ADC1_3, ADC1_4, ADC1_5
 };
 
+enum class MOTOR_PINS : uint8_t {
+    IN1 = 13, IN2 = 14, IN3 = 15, IN4 = 17
+};
+
+enum class Direction : bool {
+    FORWARD = 0, BACKWARD = 1
+};
+
 struct IR {
     constexpr IR(const uint8_t trig_pin, const ADC_PINS pin) : trig_pin(trig_pin),
     echo_pin(pin) {}
@@ -17,7 +25,6 @@ struct IR {
     }
 
     //TODO: Enter a critical section
-
     uint16_t read() const {
         digitalWrite(this->trig_pin, HIGH);
         delayMicroseconds(5);
@@ -25,6 +32,25 @@ struct IR {
         digitalWrite(this->trig_pin, LOW);
         return val;
     }
+};
+
+struct Motor {
+    Motor(const MOTOR_PINS in1, const MOTOR_PINS in2) : IN1(in1), IN2(in2) {
+        pinMode(static_cast<uint8_t>(IN1), OUTPUT);
+        pinMode(static_cast<uint8_t>(IN2), OUTPUT);
+        analogWriteResolution(static_cast<uint8_t>(this->IN1), 10);
+        analogWriteResolution(static_cast<uint8_t>(this->IN2), 10);
+        this->move(0);
+    }
+    //0 to 1024
+    void move(const uint16_t duty, const Direction direction = Direction::FORWARD) const  {
+        analogWrite(static_cast<uint8_t>(this->IN1), duty * static_cast<uint8_t>(direction));
+        analogWrite(static_cast<uint8_t>(this->IN2), duty * !static_cast<uint8_t>(direction));
+    }
+
+    private:
+    MOTOR_PINS IN1;
+    MOTOR_PINS IN2;
 };
 
 constexpr IR front_left_ir = {36, ADC_PINS::ADC1_0};
@@ -36,54 +62,17 @@ constexpr IR right_diagonal_ir = {34, ADC_PINS::ADC1_4};
 
 constexpr std::array ir_array = {front_left_ir, front_right_ir, left_ir, right_ir, left_diagonal_ir, right_diagonal_ir};
 
+
+
 void setup() {
-    Serial.begin(115200);
     for (const auto &ir : ir_array)
         ir.setup();
-
-    Serial.println("Test");
+    const auto right_motor = Motor(MOTOR_PINS::IN1, MOTOR_PINS::IN2);
+    const auto left_motor = Motor(MOTOR_PINS::IN4, MOTOR_PINS::IN3);
+    right_motor.move(1024);
+    left_motor.move(1024);
 }
 
 void loop() {
-    // for (const auto &[trig_pin, echo_pin] : ir_array) {h
-    //     digitalWrite(trig_pin, HIGH);
-    //     delay(100);
-    //     Serial.println(analogRead(echo_pin));
-    //     digitalWrite(trig_pin, LOW);
-    // }
-    for (const auto &[trig_pin, echo_pin] : ir_array) {
-        u_long a = micros();
-        digitalWrite(trig_pin, HIGH);
-        delayMicroseconds(5);
-        int val = analogRead();
-        digitalWrite(trig_pin, LOW);
-        u_long b = micros();
-        delay(100);
-        Serial.print(String(val)+" ");
-    }
-    Serial.println();
-
-
-    // digitalWrite(front_right_ir.trig_pin, HIGH);
-    // delay(10);
-    // int true_value = analogRead(front_right_ir.echo_pin);
-    // digitalWrite(front_right_ir.trig_pin, LOW);
-    // delay(10);
-    // digitalWrite(front_right_ir.trig_pin, HIGH);
-    // u_long init_time = micros();
-    // while (abs(analogRead(front_right_ir.echo_pin) - true_value) >= 10){}
-    //
-    // u_long final_time = micros();
-    // Serial.println(String(final_time - init_time));
-    // delay(1000);
-    // static int ir_output[6] = {};
-    //
-    // for (uint8_t i = 0; i < 6; i++) {
-    //     digitalWrite(ir_array [0].trig_pin, HIGH);
-    //     delayMicroseconds(final_time-init_time);
-    //     ir_output[i] = analogRead(ir_array [i].trig_pin);
-    //     digitalWrite(ir_array [i].trig_pin, LOW);
-    // }
-    // Serial.printf("%d",final_time - init_time);
 
 }
