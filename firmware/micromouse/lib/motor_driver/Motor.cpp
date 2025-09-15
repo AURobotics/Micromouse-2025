6e0eacbbfb1cf5e05d4deb3d943ce7548324ff85
minawashere <minasalama2580@gmail.com>
2025-09-15 16:38:22 +0300

    mehmeh^9

--- firmware/micromouse/lib/motor_driver/Motor.cpp
+++ firmware/micromouse/lib/motor_driver/Motor.cpp
@@ -7,15 +7,16 @@ Motor::Motor(const MOTOR_PINS in1, const MOTOR_PINS in2) : IN1(in1), IN2(in2) {
     analogWriteResolution(static_cast<uint8_t>(this->IN2), 10);
     this->move(0);
 }
-    //duty 0 to 1024, STOP sets both outputs to HIGH
+// duty 0 to 1024, STOP sets both outputs to HIGH
 void Motor::move(const uint16_t duty, const Direction direction) const {
     if (direction == Direction::FORWARD || direction == Direction::BACKWARD) {
-     analogWrite(static_cast<uint8_t>(this->IN1), duty * static_cast<uint8_t>(direction));
-     analogWrite(static_cast<uint8_t>(this->IN2), duty * !static_cast<uint8_t>(direction));
-    } else {
-     analogWrite(static_cast<uint8_t>(this->IN1), 1024);
-     analogWrite(static_cast<uint8_t>(this->IN2), 1024);
+        analogWrite(static_cast<uint8_t>(this->IN1),
+                    duty * static_cast<uint8_t>(direction));
+        analogWrite(static_cast<uint8_t>(this->IN2),
+                    duty * !static_cast<uint8_t>(direction));
+    }
+    else {
+        analogWrite(static_cast<uint8_t>(this->IN1), 1024);
+        analogWrite(static_cast<uint8_t>(this->IN2), 1024);
     }
 }
-
-
--- firmware/micromouse/src/main.cpp
+++ firmware/micromouse/src/main.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 
+#include "driver/pulse_cnt.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/pulse_cnt.h"
 
-#include "Motor.h"
-#include "IR.h"
 #include "BNO055.h"
+#include "IR.h"
+#include "Motor.h"
 #include "Wire.h"
 
 
@@ -16,15 +16,20 @@ constexpr IR left_ir = {35, ADC_PINS::ADC1_2};
 constexpr IR right_ir = {18, ADC_PINS::ADC1_3};
 constexpr IR left_diagonal_ir = {40, ADC_PINS::ADC1_1};
 constexpr IR right_diagonal_ir = {34, ADC_PINS::ADC1_4};
-constexpr std::array ir_array = {front_left_ir, front_right_ir, left_ir, right_ir, left_diagonal_ir, right_diagonal_ir};
+constexpr std::array ir_array = {front_left_ir,
+                                 front_right_ir,
+                                 left_ir,
+                                 right_ir,
+                                 left_diagonal_ir,
+                                 right_diagonal_ir};
 
 const auto right_motor = Motor(MOTOR_PINS::IN1, MOTOR_PINS::IN2);
 const auto left_motor = Motor(MOTOR_PINS::IN4, MOTOR_PINS::IN3);
 
 
-//set the default axis
-//signs corresponds to default axis
- constexpr auto bno_axis_config = Axis_remap_config{
+// set the default axis
+// signs corresponds to default axis
+constexpr auto bno_axis_config = Axis_remap_config{
     .x_axis = remap_axis::z_axis,
     .y_axis = remap_axis::y_axis,
     .z_axis = remap_axis::x_axis,
@@ -34,11 +39,12 @@ const auto left_motor = Motor(MOTOR_PINS::IN4, MOTOR_PINS::IN3);
     .z_sign = remap_sign::negative,
 };
 
-auto bno = imu(16,21, I2C_NUM_0);
+auto bno = imu(16, 21, I2C_NUM_0);
 
 float normalize_angle(const float angle, const float init_angle) {
     float ret = fmod(angle + 180 - init_angle, 360.0f);
-    if (ret < 0) ret += 360;
+    if (ret < 0)
+        ret += 360;
     ret -= 180;
     return ret * -1;
 }
@@ -53,9 +59,10 @@ void setup() {
 
 void loop() {
     auto vec = bno.euler();
-    static auto x_init = vec.x();
-    float angle = fmod(vec.z() + 180 - x_init, 360.0f);
-    if (angle < 0) angle += 360;
+    static auto z_init = vec.z();
+    float angle = fmod(vec.z() + 180 - z_init, 360.0f);
+    if (angle < 0)
+        angle += 360;
     angle -= 180;
     angle *= -1;
     Serial.println(angle);
